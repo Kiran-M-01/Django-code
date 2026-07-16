98cb670167ddee7ecdc3263fc1e126f4e96c5d8a
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-07-16 20:09:40 +0530

    sent all records from table to frontend

--- db_pro/database_project/app1/urls.py
+++ db_pro/database_project/app1/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('all_students/', views.all_students),
+    path('all_emps/',views.all_employees),
 ]
--- db_pro/database_project/app1/views.py
+++ db_pro/database_project/app1/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from .models import Student,Teacher
+from .models import Student,Teacher,Employee
 
 # Create your views here.
 
+# FOR STUDENT TABLE
 def all_students(request):
     context = {
         'Students' : Student.objects.all()
     }
     return render(request,'all_students.html', context)
+
+
+# FOR EMPLOYEE TABLE
+def all_employees(request):
+    context = {
+        "Employees" : Employee.objects.all()
+    }
+    return render(request,'all_employees.html', context)
