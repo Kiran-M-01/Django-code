640bc73d9cea364fba962cba14e535cebe2a5f0c
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-07-21 16:46:59 +0530

    crud operaton through admin

--- db_pro/database_project/app1/admin.py
+++ db_pro/database_project/app1/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Employee
 
 # Register your models here.
+
+@admin.register(Employee)
+class EmployeeModel(admin.ModelAdmin):
+    list_display = ['id','ename','salary']
+
+# admin.site.register(Employee,EmployeeModel)
+
